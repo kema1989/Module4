59fb07675ec939a7ce3ca5422cc48447da10761b
Кемран Абдрашид <kemran94875@gmail.com>
2021-07-12 20:52:09 +0600

    Добавлены все три цикла со счетчиком

--- M4U2-1/Program.cs
+++ M4U2-1/Program.cs
@@ -7,10 +7,11 @@ namespace M4U2_1
         static void Main(string[] args)
         {
             Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-
-            for (int i = 1; i < 5; i++)
+            
+            Console.WriteLine("\nЦикл for:");
+            for (int i = 0; i < 4; i++)
             {
-                Console.WriteLine("Iteration {0}", i);
+                Console.WriteLine(i);
                 switch (Console.ReadLine())
                 {
                     case "red":
@@ -38,6 +39,75 @@ namespace M4U2_1
                         break;
                 }
             }
+
+            Console.WriteLine("\n\nЦикл while:");
+            int k = 0;
+            while (k < 4)
+            {
+                Console.WriteLine(k);
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your fav color is RED!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your fav color is green!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your fav color is cyan...");
+                        break;
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Your color(yeah, just color) is yellow");
+                        break;
+                }
+                k++;
+            }
+
+            Console.WriteLine("\n\nЦикл do...while:");
+            int t = 0;
+            do
+            {
+                Console.WriteLine(t);
+                switch (Console.ReadLine())
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your fav color is RED!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your fav color is green!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your fav color is cyan...");
+                        break;
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Your color(yeah, just color) is yellow");
+                        break;
+                }
+                t++;
+            }
+            while (t < 4) ;
 
             Console.ReadKey();
         }
