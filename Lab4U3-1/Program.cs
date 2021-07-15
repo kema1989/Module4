6a3b22463c9285b08a73ba3580348f40a46e225b
Кемран Абдрашид <kemran94875@gmail.com>
2021-07-15 16:01:55 +0600

    Добавлен файл юнита 3

---
+++ Lab4U3-1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Lab4U3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите три любимых цвета на английском с маленькой буквы");
+            string[] favcolors = new string[3];
+            for(int i = 0; i < favcolors.Length; i++)
+            {
+                Console.WriteLine("Введите любимый цвет номер {0}", i + 1);
+                favcolors[i] = Console.ReadLine();
+            }
+
+            foreach(var color in favcolors)
+            {
+                switch(color)
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your fav color is RED!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your fav color is green!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("Your fav color is cyan...");
+                        break;
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Your color(yeah, just color) is yellow");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Lab4U3-2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab4U3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя");
+            var name = Console.ReadLine();
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                Console.Write(name[i] + " ");
+            }
+        }
+    }
+}
